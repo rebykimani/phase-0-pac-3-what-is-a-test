f45470a6a6227dccd3a367cc433ac5cba627eec5
beckireby15@gmail.com <beckireby15@gmail.com>
2022-08-16 01:02:02 +0300

    test

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
+// else if is used when you have multiple conditions;
+//if else is used when a condition gives either true or false;
+// else is used when we have one condition; else {canDrive = true;} 
 const name = "Joe";
 const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+console.log (message);
+// const age = 20;
+// let isAdult, canWork, canEnlist, canDrink;
+// if (age >= 21) {
+//   canWork = true;
+//   canEnlist = true;
+//   isAdult = true;
+//   canDrink = true;
+// } else if (age >= 18) {
+//   canWork = true;
+//   canEnlist = true;
+//   isAdult = true;
+// } else if (age >= 16) {
+//   canWork = true;
+// }
+
+
+// canWork;
+// canEnlist;
+// isAdult;
+// canDrink;
+// console.log(canWork, canDrink, canEnlist, isAdult);
